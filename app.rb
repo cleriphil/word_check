60fe5f2cdbe54b75423cf74b50cc4b31a0ea4407
cleriphil <cleri.phil@gmail.com>
2015-04-24 11:09:58 -0700

    Pass integration spec for path of word_check

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/coin')
+require('./lib/word_check')
 also_reload('lib/**/*.rb')
+
+
+get('/') do
+  erb(:index)
+end
+
+get('/results') do
+  @word = params.fetch('word')
+  @sentence = (params.fetch('sentence')).word_check(@word)
+  erb(:results)
+end
--- spec/word_check_integration_spec.rb
+++ spec/word_check_integration_spec.rb
@@ -1,14 +1,15 @@
 require('capybara/rspec')
 require('./app')
-Capybara = Sinatra::Application
+Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 
 describe('path for word_check', {:type => :feature}) do
-  it() do
+  it("processes the user input and returns the frequency of the word in the sentence") do
   visit('/')
-  fill_in() with
-  click_button()
-  expect(page).to have_content()
+  fill_in('word', :with => 'many')
+  fill_in('sentence', :with => 'There are many many many many trees here')
+  click_button('Enter')
+  expect(page).to have_content(4)
   end
 end
