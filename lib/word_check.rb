2f0e164eebbf7683f54bedccebdb314933f1a18f
cleriphil <cleri.phil@gmail.com>
2015-04-24 11:17:52 -0700

    Add and pass spec for when the object input is has capital letters

--- lib/word_check.rb
+++ lib/word_check.rb
@@ -1,7 +1,8 @@
 class String
   define_method(:word_check) do |given_word|
     original_sentence = self
-    original_array = self.split()
+    original_sentence = original_sentence.downcase()
+    original_array = original_sentence.split()
     original_length = original_array.length()
     original_array.delete(given_word)
     newer_length = original_array.length()
--- spec/word_check_spec.rb
+++ spec/word_check_spec.rb
@@ -3,6 +3,7 @@ require('word_check')
 
 #returns the frequency of the argument in the object when the frequency > 1.
 #returns the frequency of the argument in the object when the frequency = 0.
+#returns the frequency of the argument in the object in a sentence with capital letters.
 
 describe('String#word_check') do
   it("returns the frequency of the argument in the object when the frequency > 1.") do
@@ -11,4 +12,7 @@ describe('String#word_check') do
   it("returns the frequency of the argument in the object when the frequency = 0") do
    expect("nancy hates coffee".word_check("loves")).to(eq(0))
  end
+  it("returns the frequency of the argument in the object in a sentence with capital letters") do
+    expect("nancy Hates hates Hates coffee".word_check("hates")).to(eq(3))
+  end
 end
