1b1f355007e3f2663d292db5ee996ee872f1d093
cleriphil <cleri.phil@gmail.com>
2015-04-24 11:50:50 -0700

    Add and pass spec for when an argument has punctuation

--- spec/word_check_spec.rb
+++ spec/word_check_spec.rb
@@ -6,6 +6,7 @@ require('word_check')
 #returns the frequency of the argument in an object with capital letters.
 #returns the frequency of the argument in an object with punctuation.
 #returns the frequency of an argument with capital letters in the object.
+#returns the frequency of an argument with punctuation in the object.
 
 describe('String#word_check') do
   it("returns the frequency of the argument in the object when the frequency > 1.") do
@@ -22,5 +23,8 @@ describe('String#word_check') do
   end
   it("returns the frequency of an argument with capital letters in the object") do
     expect("Nancy likes likes likes coffee".word_check("LIKes")).to(eq(3))
+  end
+  it("returns the frequency of an argument with punctuation in the object") do
+    expect("Nancy likes likes likes likes coffee.".word_check("$$Likes$$")).to(eq(4))
   end
 end
