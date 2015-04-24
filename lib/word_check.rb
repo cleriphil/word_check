33badf3a4dbfc27140af21b730784f5c25fd982b
cleriphil <cleri.phil@gmail.com>
2015-04-24 11:45:18 -0700

    Add and pass spec for when an argument has capital letters

--- lib/word_check.rb
+++ lib/word_check.rb
@@ -4,6 +4,7 @@ class String
     original_sentence = original_sentence.downcase.gsub(/[^a-z0-9\s]/,'')
     original_array = original_sentence.split()
     original_length = original_array.length()
+    given_word = given_word.downcase()
     original_array.delete(given_word)
     newer_length = original_array.length()
     original_length.-(newer_length)
