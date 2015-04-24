484c3d25762c522b698b249a9931367add42a4e5
cleriphil <cleri.phil@gmail.com>
2015-04-24 10:27:50 -0700

    Add and pass spec that returns the frequency of the argument in the object when the frequency = 0

--- spec/word_check_spec.rb
+++ spec/word_check_spec.rb
@@ -4,10 +4,13 @@ require('word_check')
 
 
 #returns the frequency of the argument in the object when the frequency > 1.
-#(I am committing this spec again, because I accidentally did 'git add .' before I edited the spec file)
+#returns the frequency of the argument in the object when the frequency = 0.
 
 describe('String#word_check') do
   it("returns the frequency of the argument in the object when the frequency > 1.") do
     expect("nancy loves loves loves coffee".word_check("loves")).to(eq(3))
+ end
+  it("returns the frequency of the argument in the object when the frequency = 0") do
+   expect("nancy hates coffee".word_check("loves")).to(eq(0))
  end
 end
